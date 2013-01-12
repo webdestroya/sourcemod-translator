dc58da39270a6a3748ecdecd2c2db2ef71c60547
Mitch Dempsey <mrdempsey@gmail.com>
2013-01-11 16:56:57 -0800

    fancy parse

--- app/helpers/translations_helper.rb
+++ app/helpers/translations_helper.rb
@@ -1,2 +1,27 @@
 module TranslationsHelper
+
+  def fancy_text(translation)
+    text = translation.text
+    if translation.phrase.format
+      if translation.phrase.format_infos.count > 0
+        translation.phrase.format_infos.each do |fmt|
+          text.gsub!(/(\{#{fmt.position}\})/, "<strong rel=\"tooltip\" title=\"TEST\">\\1</strong>")
+        end
+      else
+        
+        text.gsub!(/(\%[dixfsct])/, "<strong>\\1</strong>")
+      end
+    end
+
+
+    text = colorize_translation(text)
+
+    content_tag(:span, text.html_safe, class: "phrase-text fancy-text")
+  end
+
+  def colorize_translation(text)
+    text.gsub! /(?:{([a-z]+)})/, "</span><span class=\"color-\\1\">{\\1}"
+    "<span class=\"colorized\"><span>#{text}</span></span>"
+  end
+
 end
