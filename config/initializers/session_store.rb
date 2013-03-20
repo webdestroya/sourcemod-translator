c399e3ace81db0e6c44d24dea7bd5bc584372d06
Mitch Dempsey <mrdempsey@gmail.com>
2013-03-20 11:50:37 -0700

    Switching from RedisToGo to RedisCloud

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,5 @@
 
 #SourcemodTranslator::Application.config.session_store :cookie_store, key: '_sourcemod_translator_session'
 
-SourcemodTranslator::Application.config.session_store :redis_store, :servers => ENV['REDISTOGO_URL']
+# SourcemodTranslator::Application.config.session_store :redis_store, :servers => ENV['REDISTOGO_URL']
+SourcemodTranslator::Application.config.session_store :redis_store, :servers => ENV['REDISCLOUD_URL']
